041f470a3bf2528d0861bccb87107b83de1718eb
Jakubhl <kuba.hlavacek567@seznam.cz>
2026-04-29 21:49:03 +0200

    remove build folder

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ToDoManager_FILE C:/Users/kubah/Desktop/github_personal_utilities/ToDoManager/build/ToDoManager.exe)
-set(__QT_DEPLOY_TARGET_ToDoManager_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ToDoManager_RUNTIME_DLLS C:/Qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Core.dll)
--- build/CMakeFiles/ToDoManager.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ToDoManager.
-# This may be replaced when dependencies are built.
